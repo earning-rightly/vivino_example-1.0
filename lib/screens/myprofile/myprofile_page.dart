c384d4548be8f06ea5d0394776f5876ab23fc0da
sung <reputable4126@gmail.com>
2023-05-13 21:48:42 +0900

    <refactory> : <Update file paths for screens>
    main.dart와 style폴더 위치는 그대로 두었습니다.
    각 페이지마다 기능을 구현하려고 하였는데 생각보다 많은 기능을 넣어야
    할거 같았고 그러면 파일이 늘어날 것이라는 생각이 들어서 미리 파일을
    효율적으로 분할하기 위해서 나누어 두었습니다.
    
    < 변동 사항>
        1. lib\friends_page.dart => lib\screens\friends\friends_page.dart
        2. lib\myprofile_page.dart => lib\screens\myprofile\myprofile_page.dart
        3. lib\search_page.dart => lib\screens\search\search_page.dart
        4. lib\top_list_page.dart => lib\screens\top_list\top_list_page.dart

---
+++ lib/screens/myprofile/myprofile_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class MyPrpfilePage extends StatefulWidget {
+  const MyPrpfilePage({Key? key}) : super(key: key);
+
+  @override
+  State<MyPrpfilePage> createState() => _MyPrpfilePageState();
+}
+
+class _MyPrpfilePageState extends State<MyPrpfilePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
