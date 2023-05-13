9c84906c27ac6be359dd96019848bd0595ec2d27
sung <reputable4126@gmail.com>
2023-05-13 20:21:17 +0900

    <feature> : <add a widget of TopChartInContry >
    TopChartInContry는 user가 있는 나라에서 유명한 와인을 toplist로
    보여주는 기능을 실행합니다. vivino에서 있는 기능을 구현한 것으로
    GPS로 지금 user가 있는 나라에 와인들중 가격으로 구간을 나눠서 와인
    상품을 추천해 주는 기능을 가지고 있습니다.

--- lib/top_list_page.dart
+++ lib/top_list_page.dart
@@ -14,6 +14,49 @@ class TopListPage extends StatelessWidget {
               'assets/top_list_page_image/wines.jpg',
               fit: BoxFit.contain,
             )),
+        Expanded(child: TopChartsInContry()),
+      ],
+    );
+  }
+}
+
+class TopChartsInContry extends StatefulWidget {
+  const TopChartsInContry({Key? key}) : super(key: key);
+
+  @override
+  State<TopChartsInContry> createState() => _TopChartsInContryState();
+}
+
+class _TopChartsInContryState extends State<TopChartsInContry> {
+  final List<Map<String, dynamic>> topListData = [
+    {
+      'icone': const Icon(Icons.monetization_on),
+      'title': 'Best wines under ￦20000 right now'
+    },
+    {
+      'icone': const Icon(Icons.monetization_on_sharp),
+      'title': 'Best wines between ￦20000 and ￦40000 right now'
+    },
+    {
+      'icone': const Icon(Icons.money),
+      'title': 'Best wines between ￦40000 and ￦80000 right now'
+    },
+    {
+      'icone': const Icon(Icons.money_rounded),
+      'title': 'Best wines over ￦80000 right now'
+    },
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ListTile(
+          title: Text('Top chart in 대한민국'),
+        ),
+        Expanded(
+            child: ListView.builder(
+                itemCount: topListData.length, itemBuilder: (c, i) {})),
       ],
     );
   }
