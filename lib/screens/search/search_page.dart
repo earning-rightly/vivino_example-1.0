09898d371c00eef77059cae3ca7d4fe9c0c8df81
sung <reputable4126@gmail.com>
2023-05-13 22:47:58 +0900

    <feat> : <add a SearchBar UI>
    search_page에서 사용할 검색창 UI를 추가하였습니다. 이 검색창에
    검색을하려고 누르면 검색 페이지로 이동하는 기능을 구현할 것입니다.
    ``` flutter
    class SearchBar extends StatefulWidget {
      const SearchBar({super.key});
    
      @override
      State<SearchBar> createState() => _SearchBarState();
    }
    
    ```

--- lib/screens/search/search_page.dart
+++ lib/screens/search/search_page.dart
@@ -30,8 +30,38 @@ class SearchBar extends StatefulWidget {
 }
 
 class _SearchBarState extends State<SearchBar> {
+  final TextEditingController _searchController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        // Add padding around the search bar
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        // Use a Material design search bar
+        child: TextField(
+          controller: _searchController,
+          decoration: InputDecoration(
+            hintText: 'Search any wine',
+            // Add a clear button to the search bar
+            suffixIcon: IconButton(
+              icon: Icon(Icons.clear),
+              onPressed: () => _searchController.clear(),
+            ),
+            // Add a search icon or button to the search bar
+            prefixIcon: IconButton(
+              icon: Icon(Icons.search),
+              onPressed: () {
+                // Perform the search here
+              },
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+        ),
+      ),
+    );
   }
 }
