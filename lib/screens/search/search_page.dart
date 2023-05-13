28fbc368c02b9c8834c5e0a719987cc2a987ab69
sung <reputable4126@gmail.com>
2023-05-13 22:28:42 +0900

    <feat> : <add ListBiew.builder on SearchPage Widget>
    search_page.dart에 있는 가장큰 틀인 SerahPage widget 안에
    Listview.builder를 추가했습니다. Listview.bduilder를 추가한 이유는
    Search.page에는 검색뿐만 아니라 user가 보기 좋은 wine catarlog를 만들기
    위해서 여거개의 widget을 추가 할것입니다. 그러면 scroll을 사용하게 되기
    때문에 Listview.builder를 사용하게 되었습니다.
    
    - Listvire.builder의 itemCount 4를 둔 것은 임시로 둔 것이며 수정될
      예정입니다
    - SerachBar Widget도 앞으로 세부적인 내뇽을 추가할겠습니다.

--- lib/screens/search/search_page.dart
+++ lib/screens/search/search_page.dart
@@ -10,6 +10,26 @@ class SearchPage extends StatefulWidget {
 class _SearchPageState extends State<SearchPage> {
   @override
   Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 4,
+        itemBuilder: (BuildContext context, int index) {
+          if (index == 0) {
+            return SearchBar();
+          }
+        });
+  }
+}
+
+class SearchBar extends StatefulWidget {
+  const SearchBar({super.key});
+
+  @override
+  State<SearchBar> createState() => _SearchBarState();
+}
+
+class _SearchBarState extends State<SearchBar> {
+  @override
+  Widget build(BuildContext context) {
     return const Placeholder();
   }
 }
