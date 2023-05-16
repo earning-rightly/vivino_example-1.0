3d78659fee41fca270e82d4ee30412dc93caa04a
sung <reputable4126@gmail.com>
2023-05-16 14:58:16 +0900

    <feat> : <Add Search UI on DetailSearchPage's appBar>
    Search UI에 디자인은 추후 변동가능합니다.
    appBar 왼쪽 상단에 뒤로 가기 버튼은 Ios에서만 사용할 예정입니다.
    andrioid에서는 bottom에 있는 뒤로가기 버튼을 사용할 것입니다.
    
    <추가 사항>
     - Search UI를 구현
     - 뒤로가기 기능  appBar 왼쪽 상단에 구현
     - 현재 검색 단어 삭제 기능 구현

--- lib/screens/search/search_page.dart
+++ lib/screens/search/search_page.dart
@@ -72,10 +72,48 @@ class _SearchBarState extends State<SearchBar> {
 }
 
 class DetailSearchPage extends StatelessWidget {
-  const DetailSearchPage({super.key});
+  DetailSearchPage({super.key});
+  final TextEditingController _searchController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+        appBar: AppBar(
+      title: Container(
+        // Add padding around the search bar
+        padding: const EdgeInsets.symmetric(horizontal: 8.0),
+        // Use a Material design search bar
+        child: TextField(
+          cursorColor: Colors.black,
+          controller: _searchController,
+          decoration: InputDecoration(
+            filled: true,
+            fillColor: Colors.white,
+            hintText: 'Search...',
+            // Add a clear button to the search bar
+            suffixIcon: IconButton(
+              icon: Icon(
+                Icons.clear,
+                color: Colors.black,
+              ),
+              onPressed: () => _searchController.clear(),
+            ),
+            // Add a search icon or button to the search bar
+            prefixIcon: IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.black,
+              ),
+              onPressed: () {
+                // Perform the search here
+              },
+            ),
+            border: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20.0),
+            ),
+          ),
+        ),
+      ),
+    ));
   }
 }
