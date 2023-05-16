6c32358964cf40499414afb3aef90a4a288e5d0a
sung <reputable4126@gmail.com>
2023-05-16 16:13:09 +0900

    <feat> : <add a search TabBar on serach_page.dart>
    DetailSearchPage 위젯안에 tabBar를 작성하여서 추가하였다. 3개의 tab이
    있다. (All Wines, My Wines, People)이제 새로운 기능들을 추가할 것이다

--- lib/screens/search/search_page.dart
+++ lib/screens/search/search_page.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
+import 'all wines/all_wines.dart';
+import 'my wines/my_wines.dart';
+import 'people/people.dart';
 
 class SearchPage extends StatefulWidget {
   const SearchPage({Key? key}) : super(key: key);
@@ -78,42 +81,62 @@ class DetailSearchPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        appBar: AppBar(
-      title: Container(
-        // Add padding around the search bar
-        padding: const EdgeInsets.symmetric(horizontal: 8.0),
-        // Use a Material design search bar
-        child: TextField(
-          cursorColor: Colors.black,
-          controller: _searchController,
-          decoration: InputDecoration(
-            filled: true,
-            fillColor: Colors.white,
-            hintText: 'Search...',
-            // Add a clear button to the search bar
-            suffixIcon: IconButton(
-              icon: Icon(
-                Icons.clear,
-                color: Colors.black,
+      appBar: AppBar(
+        title: Container(
+          // Add padding around the search bar
+          padding: const EdgeInsets.symmetric(horizontal: 8.0),
+          // Use a Material design search bar
+          child: TextField(
+            cursorColor: Colors.black,
+            controller: _searchController,
+            decoration: InputDecoration(
+              filled: true,
+              fillColor: Colors.white,
+              hintText: 'Search...',
+              // Add a clear button to the search bar
+              suffixIcon: IconButton(
+                icon: Icon(
+                  Icons.clear,
+                  color: Colors.black,
+                ),
+                onPressed: () => _searchController.clear(),
               ),
-              onPressed: () => _searchController.clear(),
-            ),
-            // Add a search icon or button to the search bar
-            prefixIcon: IconButton(
-              icon: Icon(
-                Icons.search,
-                color: Colors.black,
+              // Add a search icon or button to the search bar
+              prefixIcon: IconButton(
+                icon: Icon(
+                  Icons.search,
+                  color: Colors.black,
+                ),
+                onPressed: () {
+                  // Perform the search here
+                },
               ),
-              onPressed: () {
-                // Perform the search here
-              },
-            ),
-            border: OutlineInputBorder(
-              borderRadius: BorderRadius.circular(20.0),
+              border: OutlineInputBorder(
+                borderRadius: BorderRadius.circular(20.0),
+              ),
             ),
           ),
         ),
       ),
-    ));
+      body: DefaultTabController(
+          length: 3,
+          child: Column(
+            children: [
+              TabBar(
+                labelColor: Colors.black,
+                unselectedLabelColor: Colors.grey,
+                tabs: [
+                  Tab(text: 'All Wines'),
+                  Tab(text: 'My Wines'),
+                  Tab(text: 'People'),
+                ],
+              ),
+              Expanded(
+                child: const TabBarView(
+                    children: [AllWines(), MyWines(), People()]),
+              )
+            ],
+          )),
+    );
   }
 }
